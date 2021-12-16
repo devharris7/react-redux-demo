1ea2bb60c72dadabbab42d856db3a3e5cc57f991
devharris7 <harris.wang@mail.com>
2021-12-16 20:05:36 +1100

    feat: useDispatch and useSelector

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   addOne,
   substractOne,
@@ -9,60 +9,72 @@ import {
 } from "./actions/counter";
 import { changeGreeting } from "./actions/greeting";
 
-const AppComponent = ({
-  greeting,
-  counter,
-  onChange,
-  add,
-  addAnyNumber,
-  substract,
-  reset,
-  asyncIncrement,
-}) => {
-  // useSelector((state) => {
-  //   console.log(state);
-  //   return state.greeting;
-  // });
+const AppComponent = () => {
+  const greeting = useSelector((state) => state.greeting);
+  const counter = useSelector((state) => state.counter);
 
   const { text, name } = greeting;
-  // const [greeting, setGreeting] = useState({ text: "", name: "" });
+
   const [number, setNumber] = React.useState(0);
-  console.log(number);
+
+  const dispatch = useDispatch();
+
+  const handleNameInputChange = (e) => {
+    dispatch(changeGreeting(e.target.value));
+  };
+
+  const handleAddOne = () => {
+    dispatch(addOne());
+  };
+
+  const handleAddAny = () => {
+    dispatch(addNumber(Number(number)));
+  };
+
+  const handleSubstractOne = () => {
+    dispatch(substractOne());
+  };
+
+  const handleReset = () => {
+    dispatch(_reset());
+  };
+
+  const handleAsyncIncrement = () => {
+    dispatch(_asyncIncrement());
+  };
 
   return (
     <div className="index">
       <p>{text}</p>
-      <input defaultValue={name} onChange={onChange} />
+      <input defaultValue={name} onChange={handleNameInputChange} />
       <p>{counter}</p>
-      <button onClick={add}>Plus 1</button>
+      <button onClick={handleAddOne}>Plus 1</button>
       <input onChange={(e) => setNumber(e.target.value)} />
-      <button onClick={() => addAnyNumber(Number(number))}>
-        Add Any Number
-      </button>
-      <button onClick={substract}>Minus 1</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={asyncIncrement}>Plus 1 after 1 sec</button>
+      <button onClick={handleAddAny}>Add Any Number</button>
+      <button onClick={handleSubstractOne}>Minus 1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleAsyncIncrement}>Plus 1 after 1 sec</button>
     </div>
   );
 };
 
 // Map Redux state to component props
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    greeting: state.greeting,
-    counter: state.counter,
-  };
-};
+// const mapStateToProps = (state) => {
+//   console.log(state);
+//   return {
+//     greeting: state.greeting,
+//     counter: state.counter,
+//   };
+// };
 
-// Map Redux actions to component props
-const mapDispatchToProps = {
-  onChange: changeGreeting,
-  add: addOne,
-  addAnyNumber: addNumber,
-  substract: substractOne,
-  reset: _reset,
-  asyncIncrement: _asyncIncrement,
-};
+// // Map Redux actions to component props
+// const mapDispatchToProps = {
+//   onChange: changeGreeting,
+//   add: addOne,
+//   addAnyNumber: addNumber,
+//   substract: substractOne,
+//   reset: _reset,
+//   asyncIncrement: _asyncIncrement,
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(AppComponent);
+export default AppComponent;
